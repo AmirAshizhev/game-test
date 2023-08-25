bf4e091e33fe6bd0403b1ef44eba1ca9caf2b69b
Amir Ashizhev <amirashizhev@yandex.ru>
2023-08-25 20:41:05 +0300

    feat: полуилось сделать так, чтобы кролики появлялись уже до 5 штук

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ let textStyle = new PIXI.TextStyle({
 let score = new PIXI.Text("Score: 0", textStyle);
 let value = 0;
 
-// lфункция рандомной х координаты для падающего кролика
+// функция рандомной х координаты для падающего кролика
 function randomBunnyPosition() {
   return app.screen.width / 2 + randomPosition(-250, 225);
 }
@@ -33,9 +33,14 @@ function createBunny() {
 const mainBunny = createBunny();
 const fallenBunny = createBunny();
 
+//создадим массив падающих кроликов
+const fallenBunnies = [];
+fallenBunnies[0] = fallenBunny;
+console.log(fallenBunnies)
+
 // Сцена - контейнер, которые является корнем графа сцены, доавляем на нее зайцев и счет
 app.stage.addChild(mainBunny);
-app.stage.addChild(fallenBunny);
+app.stage.addChild(fallenBunnies[0]);
 app.stage.addChild(score);
 
 // Полжение плашки Score
@@ -62,23 +67,58 @@ document.addEventListener("keypress", (event) => {
 });
 
 // начальное движение и положение падающего кролика
-fallenBunny.y = 30;
-fallenBunny.x = randomBunnyPosition();
+// fallenBunny.y = 30;
+// fallenBunny.x = randomBunnyPosition();
 // Коллизия
 
 // Listen for animate update
 app.ticker.add((delta) => {
   //скорость падения
-  fallenBunny.y += 3;
+  
   // здесь используется функция коллизий, она нужна чтобы запечатлеть момент соприкосновения и увеличить значение scor
-  if (hitTestRectangle(fallenBunny, mainBunny)) {
-    fallenBunny.y = 30;
-    fallenBunny.x = randomBunnyPosition();
 
-    value++;
-    score.text = `Score: ${value}`;
-  } else if (fallenBunny.y === app.screen.height) {
-    fallenBunny.y = 30;
-    fallenBunny.x = randomBunnyPosition();
+  for (let i = 0; i < fallenBunnies.length; i++) {
+    // fallenBunnies[fallenBunnies.length] = createBunny();
+    app.stage.addChild(fallenBunnies[i])
+    fallenBunnies[i].y += 2;
+    
+
+    //проверка соприкосновения с любым из кроликов
+    if (hitTestRectangle(fallenBunnies[i], mainBunny)) {
+      fallenBunnies[i].y = 30;
+      fallenBunnies[i].x = randomBunnyPosition();
+      value++;
+      score.text = `Score: ${value}`;
+
+      // ограничение количества кроликов
+      // if (fallenBunnies.length > 4) return;
+
+
+      //добавление кроликов с задержкой после коллизии
+      setTimeout(() => {
+        if (fallenBunnies.length > 4) return;
+        fallenBunnies[fallenBunnies.length] = createBunny();
+
+        console.log(fallenBunnies);
+      }, i*1000)
+ 
+
+  
+
+
+    } else if (fallenBunnies[i].y === app.screen.height) {
+      fallenBunnies[i].y = 30;
+      fallenBunnies[i].x = randomBunnyPosition();
+    }
   }
+  // if (hitTestRectangle(fallenBunny, mainBunny)) {
+  //   fallenBunny.y = 30;
+  //   fallenBunny.x = randomBunnyPosition();
+
+  //   value++;
+  //   score.text = `Score: ${value}`;
+  // } else if (fallenBunny.y === app.screen.height) {
+  //   fallenBunny.y = 30;
+  //   fallenBunny.x = randomBunnyPosition();
+  // }
 });
